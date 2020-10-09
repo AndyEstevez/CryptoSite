809a3fb948905aec6d4e4b86985067bb8684b9b8
unknown <andyestevez27@gmail.com>
2020-10-09 15:45:24 -0400

    added template for each coin's intial info

--- src/components/CoinList/CoinList.js
+++ src/components/CoinList/CoinList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { coinPricingURL } from '../../config'
+import './CoinList.css'
 
 export default class CoinList extends Component {
     constructor(){
@@ -29,13 +30,55 @@ export default class CoinList extends Component {
     render() {
         return (
             <div>
-                {this.state.coinList.map(function(coin){
-                    return(
-                        <div>
-                            <img src={coin.image} alt={coin.name}/>
-                        </div>
-                    )
-                })}
+                <table>
+
+                    <thead>
+                        <tr>
+                            <th> Rank </th>
+                            <th> Name </th>
+                            <th> Price </th>
+                            <th> 24h </th>
+                            <th> Market Cap </th>
+                            <th> Circulating Supply </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.state.coinList.map(function(coin){
+                            return(
+                                <tr className="coinCard" key={coin.id}>
+                                    <td className="coinRank"> {coin.market_cap_rank} </td>
+                                    <td className="coinNameContainer">
+                                        <div className="coinItem">
+                                            <img className="coinImage" src={coin.image} alt={coin.name}/>
+                                            <div>
+                                                <p className="coinName"> {coin.name} </p>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td className="coinPrice"> ${String(coin.current_price).slice(0,8)} </td>
+                                    <td className="coin24H"> {String(coin.market_cap_change_percentage_24h).slice(0, 4)}% </td>
+                                    <td className="coinMarketCap"> {(coin.market_cap).toLocaleString()} </td>
+                                    <td className="coinSupply"> {(coin.circulating_supply).toLocaleString()}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+                  
+                {/* <div>
+                    {this.state.coinList.map(function(coin){
+                        return(
+                            <div className="coinCard" key={coin.id}>
+                                
+                                <div className="coinName"> {coin.name} </div>
+                                <img className="coinImage" src={coin.image} alt={coin.name}/>
+                                
+                            </div>
+                        )
+                    })}
+                </div> */}
+                
             </div>
         )
     }
